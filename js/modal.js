51360677bbd1aba8f5da11035e5b56959551759c
Svin07 <111698482+Svin07@users.noreply.github.com>
2023-04-24 10:37:40 +0300

    DZ_7_4

--- js/modal.js
+++ js/modal.js
@@ -8,7 +8,18 @@
   refs.openModalBtn.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
+    //* Закривання по кліку на бекдроп
+
+  refs.modal.addEventListener('click', removeModal);
+  function removeModal(e) {
+    if (e.target === e.currentTarget) {
+      refs.modal.classList.add('is-hidden');
+      document.body.classList.remove('no-scroll');
+    }
+  }
+
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
+    document.body.classList.toggle('no-scroll');
   }
 })();
